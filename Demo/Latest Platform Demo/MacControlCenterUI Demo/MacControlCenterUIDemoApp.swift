71c8cab53d0eef29a5c9dbd1e787ed82db29605b
Steffan Andrews <stef@orchetect.com>
2023-02-08 02:16:01 -0800

    Added new MenuBarExtra demo project

---
+++ Demo/Latest Platform Demo/MacControlCenterUI Demo/MacControlCenterUIDemoApp.swift
@@ -0,0 +1,69 @@
+//
+//  AppDelegate.swift
+//  MacControlCenterUIDemoApp • https://github.com/orchetect/MacControlCenterUI
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+import SwiftUI
+import MacControlCenterUI
+
+@main
+struct MacControlCenterUIDemoApp: App {
+    //@Environment(\.scenePhase) private var scenePhase
+    
+    var body: some Scene {
+        MenuBarExtra("MacControlCenterUI Demo", systemImage: "slider.horizontal.3") {
+            Menu()
+        }
+        .menuBarExtraStyle(.window)
+    }
+}
+
+struct Menu: View {
+    @State private var volume: CGFloat = 0.75
+    @State private var brightness: CGFloat = 0.5
+    @State private var item1: Bool = false
+    
+    /// Based on macOS Control Center slider width
+    let sliderWidth: CGFloat = 270
+    
+    var body: some View {
+        MacControlCenterMenu {
+            MacControlCenterSlider(
+                "Display",
+                value: $brightness,
+                image: .macControlCenterDisplayBrightness
+            )
+            .frame(minWidth: sliderWidth)
+            
+            Divider()
+            
+            MacControlCenterVolumeSlider("Sound", value: $volume)
+                .frame(minWidth: sliderWidth)
+            
+            MacControlCenterCircleToggle(
+                "Toggle",
+                isOn: $item1,
+                image: .macControlCenterSpeaker
+            )
+            
+            // standard macOS-style button
+            Button("Test") {
+                print("Test pressed")
+            }
+            
+            Divider()
+            
+            MenuCommand("Settings...") {
+                NSApp.activate(ignoringOtherApps: true)
+                NSApp.sendAction(Selector(("showSettingsWindow:")), to: nil, from: nil)
+            }
+            
+            Divider()
+            
+            MenuCommand("Quit") {
+                NSApp.terminate(self)
+            }
+        }
+    }
+}
