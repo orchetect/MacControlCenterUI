5e7721f26b7d2160aee2c48a5c44f191b94629a2
Steffan Andrews <stef@orchetect.com>
2023-03-06 03:13:41 -0800

    Added FullWidthMenuItem

---
+++ Sources/MacControlCenterUI/Menu Builder/FullWidthMenuItem.swift
@@ -0,0 +1,37 @@
+//
+//  FullWidthMenuItem.swift
+//  MacControlCenterUI • https://github.com/orchetect/MacControlCenterUI
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+import SwiftUI
+
+/// Generic full-width ``MacControlCenterMenu`` menu entry to contain any arbitrary view.
+@available(macOS 10.15, *)
+@available(iOS, unavailable)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
+public struct FullWidthMenuItem<Content: View>: View, MacControlCenterMenuItem {
+    public var verticalPadding: Bool
+    public var content: Content
+    
+    // MARK: Init
+    
+    public init(
+        verticalPadding: Bool = true,
+        @ViewBuilder _ content: () -> Content
+    ) {
+        self.verticalPadding = verticalPadding
+        self.content = content()
+    }
+    
+    // MARK: Body
+    
+    public var body: some View {
+        VStack(alignment: .leading) {
+            content
+        }
+        .padding([.top, .bottom],
+                 verticalPadding ? MenuGeometry.menuItemPadding : 0)
+    }
+}
