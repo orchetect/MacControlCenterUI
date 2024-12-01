096a78064029c27f098b239013aae416fbb900d0
Steffan Andrews <stef@orchetect.com>
2024-11-30 16:07:46 -0800

    Added override for `disabled()` view modifier to maintain menu spacing

--- Sources/MacControlCenterUI/Menu/MacControlCenterMenuItem.swift
+++ Sources/MacControlCenterUI/Menu/MacControlCenterMenuItem.swift
@@ -12,6 +12,46 @@ import SwiftUI
 
 /// Internal use only.
 /// It is not necessary to conform your views to this protocol unless you require custom view padding in a ``MacControlCenterMenu``.
-@MainActor public protocol MacControlCenterMenuItem { }
+@MainActor public protocol MacControlCenterMenuItem where Self: View { }
+
+// MARK: - SwiftUI View Modifier Overrides to Retain Protocol Typing
+
+extension MacControlCenterMenuItem {
+    /// Return self type-erased to `some View`.
+    nonisolated var selfAsView: some View { self }
+    
+    /// Adds a condition that controls whether users can interact with this
+    /// view.
+    ///
+    /// The higher views in a view hierarchy can override the value you set on
+    /// this view. In the following example, the button isn't interactive
+    /// because the outer `disabled(_:)` modifier overrides the inner one:
+    ///
+    ///     HStack {
+    ///         Button(Text("Press")) {}
+    ///         .disabled(false)
+    ///     }
+    ///     .disabled(true)
+    ///
+    /// - Parameter disabled: A Boolean value that determines whether users can
+    ///   interact with this view.
+    ///
+    /// - Returns: A view that controls whether users can interact with this
+    ///   view.
+    nonisolated public func disabled(_ disabled: Bool) -> some MacControlCenterMenuItem {
+        MacControlCenterMenuItemWrapper(
+            content: selfAsView.disabled(disabled)
+        )
+    }
+}
+
+/// Generic wrapper for arbitrary view content to conform to ``MacControlCenterMenuItem``
+struct MacControlCenterMenuItemWrapper<Content: View>: View, MacControlCenterMenuItem {
+    let content: Content
+    
+    var body: some View {
+        content
+    }
+}
 
 #endif
