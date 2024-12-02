b0957daf2d2bba54cdb95f7c9ebe198e9fb42b2e
Steffan Andrews <stef@orchetect.com>
2024-12-02 15:34:34 -0800

    Added `MenuDisclosureChevron` view

---
+++ Sources/MacControlCenterUI/Menu Items/MenuDisclosureChevron.swift
@@ -0,0 +1,106 @@
+//
+//  MenuDisclosureChevron.swift
+//  MacControlCenterUI • https://github.com/orchetect/MacControlCenterUI
+//  © 2024 Steffan Andrews • Licensed under MIT License
+//
+
+#if os(macOS)
+
+import SwiftUI
+
+/// Disclosure chevron used in ``MacControlCenterMenu`` items.
+public struct MenuDisclosureChevron: View {
+    // MARK: Public Properties
+    
+    public let size: CGFloat
+    @Binding public var isExpanded: Bool
+    
+    // MARK: Environment
+    
+    @Environment(\.isEnabled) private var isEnabled
+    
+    // MARK: Private State
+    
+    @State private var isPressed: Bool = false
+    @State private var frame: CGRect = .zero
+    @State private var rotationAngle: Angle = .zero
+    
+    // MARK: Init
+    
+    public init(
+        size: CGFloat = 10,
+        isExpanded: Binding<Bool>
+    ) {
+        self.size = size
+        _isExpanded = isExpanded
+    }
+    
+    // MARK: Body
+    
+    public var body: some View {
+        if #available(macOS 13.0, *) {
+            imageBody
+                .gesture(gesture)
+                .onGeometryChange(for: CGRect.self) { proxy in
+                    proxy.frame(in: .local)
+                } action: { newFrame in
+                    frame = newFrame
+                }
+        } else { // TODO: delete when package min req. is bumped to macOS 13
+            GeometryReader { geometry in
+                imageBody
+                    .gesture(gesture)
+                    .onAppear { frame = geometry.frame(in: .local) }
+                    .onChange(of: geometry.frame(in: .local)) { newValue in
+                        frame = newValue
+                    }
+            }
+            .frame(width: size, height: size)
+        }
+    }
+    
+    private var imageBody: some View {
+        Image(systemName: "chevron.right")
+            .resizable()
+            .scaledToFit()
+            .frame(width: size, height: size)
+            .contentShape(Rectangle())
+            .foregroundColor(.primary)
+            .rotationEffect(rotationAngle)
+            .opacity(opacityValue)
+            // .animation(.default, value: isExpanded) // avoid, causes side effects
+        
+            .onChange(of: isExpanded) { _ in
+                handleRotation()
+            }
+    }
+        
+    private var gesture: some Gesture {
+        DragGesture(minimumDistance: 0)
+            .onChanged { value in
+                guard isEnabled else { return }
+                let hit = frame.contains(value.location)
+                isPressed = hit
+            }
+            .onEnded { value in
+                guard isEnabled else { return }
+                defer { isPressed = false }
+                if isPressed {
+                    isExpanded.toggle()
+                }
+            }
+    }
+    
+    private func handleRotation() {
+        withAnimation {
+            rotationAngle = isExpanded ? .degrees(90) : .zero
+        }
+    }
+    
+    private var opacityValue: CGFloat {
+        guard isEnabled else { return 0.4 }
+        return isPressed ? 0.7 : 1.0
+    }
+}
+
+#endif
