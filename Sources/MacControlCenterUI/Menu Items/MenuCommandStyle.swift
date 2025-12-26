c47e4c6143422b30f8b27ff08a6b233d53206df2
Steffan Andrews <stef@orchetect.com>
2025-12-25 18:13:55 -0800

    `MenuCommandStyle`: Added `plain` case

--- Sources/MacControlCenterUI/Menu Items/MenuCommandStyle.swift
+++ Sources/MacControlCenterUI/Menu Items/MenuCommandStyle.swift
@@ -20,6 +20,9 @@ public enum MenuCommandStyle {
     
     /// Control Center style (highlight on mouse hover using translucent gray color).
     case controlCenter
+    
+    /// Plain style without applying any pre-determined formatting.
+    case plain
 }
 
 extension MenuCommandStyle: Equatable { }
@@ -32,7 +35,7 @@ extension MenuCommandStyle: Sendable { }
 
 extension MenuCommandStyle {
     public func textColor(hover: Bool, isEnabled: Bool) -> Color? {
-        let base: Color = switch self {
+        let base: Color? = switch self {
         case .menu:
             hover
                 ? MenuGeometry.menuItemStandardHoverForeColor
@@ -40,9 +43,12 @@ extension MenuCommandStyle {
             
         case .controlCenter:
             .primary
+            
+        case .plain:
+            nil
         }
         
-        return base.opacity(isEnabled ? 1.0 : 0.4)
+        return base?.opacity(isEnabled ? 1.0 : 0.4)
     }
     
     public func backColor(hover: Bool, for colorScheme: ColorScheme) -> Color? {
@@ -51,12 +57,13 @@ extension MenuCommandStyle {
         switch self {
         case .menu:
             return MenuGeometry.menuItemStandardHoverBackColor
-        case .controlCenter:
+            
+        case .controlCenter, .plain:
             if colorScheme == .dark {
                 return Color(white: 0.3, opacity: 0.4)
             } else {
                 return Color(white: 0.9, opacity: 0.2)
-            }
+            }    
         }
     }
 }
