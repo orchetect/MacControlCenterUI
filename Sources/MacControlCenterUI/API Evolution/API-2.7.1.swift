c571517e5b2b9759bb108b107f6eb3d34ace6138
Steffan Andrews <stef@orchetect.com>
2025-12-25 19:57:25 -0800

    Updated API 2.7.1 Evolution

--- Sources/MacControlCenterUI/API Evolution/API-2.7.1.swift
+++ Sources/MacControlCenterUI/API Evolution/API-2.7.1.swift
@@ -9,6 +9,55 @@
 import SwiftUI
 
 extension MenuCommand {
+    @_documentation(visibility: internal)
+    @_disfavoredOverload
+    @available(*, deprecated, renamed: "init(activatesApp:action:label:)")
+    public init(
+        action: @MainActor @escaping () -> Void,
+        activatesApp: Bool,
+        @ViewBuilder label: @MainActor @escaping () -> Label
+    ) {
+        self.init(
+            activatesApp: activatesApp,
+            action: action,
+            label: label
+        )
+    }
+    
+    @_documentation(visibility: internal)
+    @_disfavoredOverload
+    @available(*, deprecated, renamed: "init(dismissesMenu:action:label:)")
+    public init(
+        action: @MainActor @escaping () -> Void,
+        dismissesMenu: Bool,
+        @ViewBuilder label: @MainActor @escaping () -> Label
+    ) {
+        self.init(
+            dismissesMenu: dismissesMenu,
+            action: action,
+            label: label
+        )
+    }
+    
+    @_documentation(visibility: internal)
+    @_disfavoredOverload
+    @available(*, deprecated, renamed: "init(activatesApp:dismissesMenu:action:label:)")
+    public init(
+        action: @MainActor @escaping () -> Void,
+        activatesApp: Bool,
+        dismissesMenu: Bool,
+        @ViewBuilder label: @MainActor @escaping () -> Label
+    ) {
+        self.init(
+            activatesApp: activatesApp,
+            dismissesMenu: dismissesMenu,
+            action: action,
+            label: label
+        )
+    }
+}
+
+extension MenuCommand {
     /// Apply a menu command style.
     @_documentation(visibility: internal)
     @available(*, deprecated, message: "This view modifier is deprecated. MenuCommand initializers now have a `style` parameter.")
